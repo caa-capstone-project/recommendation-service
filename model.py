c27c2de6ccf9d12978ac84470052d2c57bd47ad3
Mayuresh <mayureshkn99@gmail.com>
2024-07-15 18:31:49 -0400

    refactor: change architecture

--- model.py
+++ model.py
@@ -4,23 +4,13 @@ import torch.nn.functional as F
 class Recommender(nn.Module):
     def __init__(self):
         super(Recommender, self).__init__()
-        self.encoder = nn.Sequential(
-            nn.Linear(9724, 512),
+        self.model = nn.Sequential(
+            nn.Linear(9724, 4096),
             nn.ReLU(),
-            nn.Linear(512, 256),
+            nn.Linear(4096, 4096),
             nn.ReLU(),
-            nn.Linear(256, 128),
-            nn.ReLU()
-        )
-        self.decoder = nn.Sequential(
-            nn.Linear(128, 256),
-            nn.ReLU(),
-            nn.Linear(256, 512),
-            nn.ReLU(),
-            nn.Linear(512, 9724)
+            nn.Linear(4096, 9724),
         )
 
     def forward(self, x):
-        encoded = self.encoder(x)
-        decoded = self.decoder(encoded)
-        return decoded
+        return self.model(x)
